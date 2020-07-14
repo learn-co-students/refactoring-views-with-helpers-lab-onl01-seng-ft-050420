7cd6c94958b8b928f25c2e96e25e9afeb6dde79d
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-07-14 10:22:04 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if artist.nil?
+      "no artist"
+    else
+      self.artist.name
+    end
   end
 
   def artist_name=(name)
+
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
